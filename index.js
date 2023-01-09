f8a66b25b1060ab2233aa8f26bd1950f22a303c4
MatheusEl <matheuselh@gmail.com>
2023-01-09 10:45:46 -0300

    index.js changed

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-import jsonServer from 'json-server';
-const server = jsonServer.create();
-const router = jsonServer.router('index.json'); 
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200; 
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
-
+server.use(middlewares)
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
